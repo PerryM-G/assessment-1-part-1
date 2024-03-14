1778e6d04139b923c3c2dbe284cf768c6fb78870
PerryM-G <162512972+PerryM-G@users.noreply.github.com>
2024-03-14 13:22:27 +1100

    added active highlighting

---
+++ main.js
@@ -0,0 +1,10 @@
+let navbar = document.getElementById("navbar");
+let navItems = document.getElementsByClassName("navItem")
+
+for(let i = 0; i < navItems.length; i++){
+  navItems[i].addEventListener("click", function(){
+    let current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active","");
+    this.className += " active";
+  });
+}
